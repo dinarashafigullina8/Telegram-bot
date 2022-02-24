69986b82eec3c865e94bd8c557255b1dec10445c
dinara <dineirashafigulllina@gmail.com>
2022-02-24 08:46:58 +0400

    #5 Телеграм бот

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-print('hi all')
+import telebot
 
-print('hii')
-print('hello world')
+bot = telebot.TeleBot('5234828077:AAHPtABuKfSuE_lRey1WT4cV-oHIYEngTlk')
 
-print('hi')
+import requests
 
-print('hello')
+
+def get_advice():
+    res = requests.get('https://api.adviceslip.com/advice')
+    srs = res.json()
+    return srs['slip']['advice']
+
+
+def translate(text):
+    res = requests.get(f'https://fasttranslator.herokuapp.com/api/v1/text/to/text?source={text}&lang=en-ru').json()
+    if(res['data']):
+        return res['data']
+    return 'перевод временно недоступен'
+
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    advice = get_advice()
+    text= translate(advice)
+    bot.send_message(message.from_user.id, "Готовлю твое предсказание")
+    bot.send_message(message.from_user.id, f"{advice}\nПеревод: {text}")
+
+
+bot.polling()
